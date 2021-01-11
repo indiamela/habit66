ee22eb82c4264765628bc43dd50352c3b9d6c633
indiamela <taishi.kusunose@gmail.com>
2021-01-11 15:20:06 +0900

    createController

--- Habit66/Controller/createViewController.swift
+++ Habit66/Controller/createViewController.swift
@@ -7,14 +7,52 @@
 
 import UIKit
 
-class createViewController: UINavigationController {
+class createViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var wishText: UITextField!
+    var wish: String = ""
+    
+    //配列
     override func viewDidLoad() {
+
+        //nextButton
+        //グレー表示
         super.viewDidLoad()
+        wishText.delegate = self
 
         // Do any additional setup after loading the view.
     }
     
+    //キーボードを隠す
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        wish = wishText.text ?? ""
+        return true
+    }
+    
+    //テキストを一文字以上入力したら次へボタンが表示される
+    
+    //次へ
+    //配列に保存
+    
+    //完了ボタン
+    //配列の中身全てをDBに入れる
+    //FireBaseに送信
+    
+    
+    //wish
+    
+    //Outcome
+    
+    //Obstacle
+    
+    //Plan
 
     /*
     // MARK: - Navigation
